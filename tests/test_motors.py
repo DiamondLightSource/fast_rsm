a533edaf2f94d1d675c8316731c980f1d8ae3483
Brearton, Richard (DLSLtd,RAL,LSCI) <richard.brearton@diamond.ac.uk>
2022-04-07 21:17:25 +0100

    Added toy test for i07 motors

--- tests/test_motors.py
+++ tests/test_motors.py
@@ -5,8 +5,11 @@ detector position in spherical polars from motor positions.
 
 # pylint: disable=protected-access
 
+import numpy as np
 import pytest
+from numpy.testing import assert_almost_equal
 
+from RSMapper.io import i07_nexus_parser
 from RSMapper.metadata import Metadata
 from RSMapper.motors import Motors
 
@@ -27,3 +30,19 @@ def test_reflection(metadata_01: Metadata):
 
     with pytest.raises(ZeroDivisionError):
         _ = motors.detector_phi
+
+
+def test_i07_phi_theta(path_to_i07_nx_01: str,
+                       i07_beam_centre_01: tuple,
+                       i07_detector_distance_01: float):
+    """
+    Make sure that we can work out where the detector is on I07.
+    """
+    images, _ = i07_nexus_parser(path_to_i07_nx_01,
+                                 i07_beam_centre_01,
+                                 i07_detector_distance_01)
+
+    motors = images[0].motors
+
+    assert_almost_equal(motors.detector_theta, 90, 2)
+    assert_almost_equal(motors.detector_phi, np.arange(15, 75, 15), 2)
