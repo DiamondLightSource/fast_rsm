bcdd70c0e8430b595958dc8b8577e3ecfa958b45
Brearton, Richard (DLSLtd,RAL,LSCI) <richard.brearton@diamond.ac.uk>
2022-04-07 11:10:43 +0100

    Added path fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@
 The conftest file contains fixtures for RSMappers tests.
 """
 
+import os
+
 from pytest import fixture
 
 from RSMapper.metadata import Metadata
@@ -13,7 +15,7 @@ def metadata_01():
     Returns an instance of metadata with fairly arbitrarily chosen parameters.
     """
     return Metadata(
-        None, # The metadata file could really be anything, so leave it.
+        None,  # The metadata file could really be anything, so leave it.
         instrument="my_instrument",  # The name of the instrument.
         detector_distance=0.2,  # 20 cm detector distance.
         pixel_size=1e-5,  # 10 µm pixel size (easy number, not unrealistic).
@@ -21,3 +23,16 @@ def metadata_01():
         data_shape=(2000, 2000),  # A 2kx2k camera.
         beam_centre=(20, 80)  # Beam's poni is towards the top left.
     )
+
+
+@fixture
+def path_to_i07_nx():
+    """
+    Returns the path to the i07 nexus file. This is a fixture for future
+    proofing reasons (i.e. maybe one day some parsing has to be done to locate
+    the file).
+    """
+    # Make it work if we're in the base dir or the test dir.
+    if os.path.exists("tests/resources/i07-418550.nxs"):
+        return "tests/resources/i07-418550.nxs"
+    return "resources/i07-418550.nxs"
