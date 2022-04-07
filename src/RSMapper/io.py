c567977cc424fc88972acb52b387711aa91928da
Brearton, Richard (DLSLtd,RAL,LSCI) <richard.brearton@diamond.ac.uk>
2022-04-07 11:43:41 +0100

    Added my favourite hacky _try_to_find_file function

--- src/RSMapper/io.py
+++ src/RSMapper/io.py
@@ -5,6 +5,7 @@ This module contains tools used for parsing data/metadata files.
 # Because of the dumb way that values are stored in the nexusformat package.
 # pylint: disable=protected-access
 
+import os
 from typing import Union, Tuple, List
 from pathlib import Path
 
@@ -48,10 +49,99 @@ def i07_nexus_parser(path_to_nx: Union[str, Path], beam_centre: Tuple[int]) -> \
         metadata = Metadata(nx_file, "i07", detector_distance, pixel_size,
                             energy, data_shape, beam_centre)
         motors = Motors(metadata)
-        images = [Image.from_file(x, motors, metadata) for x in image_path]
+
+        # Search for images (usually necessary outside of Diamond).
+        # First make sure everything is a string (could be bytes).
+        image_path = [bytes.decode(x, 'utf-8') for x in image_path]
+        image_paths = _try_to_find_files(image_path, [path_to_nx])
+        images = [Image.from_file(x, motors, metadata) for x in image_paths]
     else:
         # It's the excalibur detector.
         raise NotImplementedError()
 
     return images, Metadata(nx_file, "i07", detector_distance, pixel_size,
                             energy, data_shape, beam_centre)
+
+
+def _try_to_find_files(filenames: List[str],
+                       additional_search_paths: List[str]):
+    """
+    Check that data files exist if the file parsed by parser pointed to a
+    separate file containing intensity information. If the intensity data
+    file could not be found in its original location, check a series of
+    probable locations for the data file.
+
+    Returns:
+        `list` of `str`:
+            List of the corrected, actual paths to the files.
+    """
+    found_files = []
+
+    # If we had only one file, make a list out of it.
+    if not hasattr(filenames, "__iter__"):
+        filenames = [filenames]
+
+    cwd = os.getcwd()
+    start_dirs = [
+        cwd,  # maybe file is stored near the current working dir
+        # To search additional directories, add them in here manually.
+    ]
+    start_dirs.extend(additional_search_paths)
+
+    local_start_directories = [x.replace('\\', '/') for x in start_dirs]
+    num_start_directories = len(local_start_directories)
+
+    # Now extend the additional search paths.
+    for i in range(num_start_directories):
+        search_path = local_start_directories[i]
+        split_srch_path = search_path.split('/')
+        for j in range(len(split_srch_path)):
+            extra_path_list = split_srch_path[:-(j+1)]
+            extra_path = '/'.join(extra_path_list)
+            local_start_directories.append(extra_path)
+
+    # This line allows for a loading bar to show as we check the file.
+    for i, _ in enumerate(filenames):
+        # Better to be safe... Note: windows is happy with / even though it
+        # defaults to \
+        filenames[i] = str(filenames[i]).replace('\\', '/')
+
+        # Maybe we can see the file in its original storage location?
+        if os.path.isfile(filenames[i]):
+            found_files.append(filenames[i])
+            continue
+
+        # If not, maybe it's stored locally? If the file was stored at
+        # location /a1/a2/.../aN/file originally, for a local directory LD,
+        # check locations LD/aj/aj+1/.../aN for all j<N and all LD's of
+        # interest. This algorithm is a generalization of Andrew McCluskey's
+        # original approach.
+
+        # now generate a list of all directories that we'd like to check
+        candidate_paths = []
+        split_file_path = str(filenames[i]).split('/')
+        for j in range(len(split_file_path)):
+            local_guess = '/'.join(split_file_path[j:])
+            for start_dir in local_start_directories:
+                candidate_paths.append(
+                    os.path.join(start_dir, local_guess))
+
+        # Iterate over each of the candidate paths to see if any of them contain
+        # the data file we're looking for.
+        found_file = False
+        for candidate_path in candidate_paths:
+            if os.path.isfile(candidate_path):
+                # File found - add the correct file location to found_files
+                found_files.append(candidate_path)
+                found_file = not found_file
+                print("Data file found at " + candidate_path + ".")
+                break
+
+        # If we didn't find the file, tell the user.
+        if not found_file:
+            raise FileNotFoundError(
+                "The data file with the name " + filenames[i] + " could "
+                "not be found. The following paths were searched:\n" +
+                "\n".join(candidate_paths)
+            )
+    return found_files
