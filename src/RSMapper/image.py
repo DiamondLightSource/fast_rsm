Mapper is mapping, units are correct!

src/RSMapper/image.py
@@ -54,29 +54,21 @@ class Image:
         return self._raw_data/self.metadata.solid_angles
 
     @property
-    def pixel_polar_lab_frame(self):
-        """
-        Returns the polar angle at each pixel, where the coordinate system is
-        tied to the lab.
-        """
-        return self.metadata.relative_polar + self.motors.detector_polar
-
-    @property
-    def pixel_azimuth_lab_frame(self):
-        """
-        Returns the azimuthal angle at each pixel, where the coordinate system
-        is tied to the lab.
-        """
-        return self.metadata.relative_azimuth + self.motors.detector_phi
-
-    @property
-    def pixel_polar_sample_frame(self):
+    def pixel_polar_angle_sample_frame(self):
         """
         Returns the polar angle at each pixel, where the coordinate system is
         tied to the sample.
         """
-        return self.metadata.relative_polar + \
-            self.motors.sample_rotation.apply(self.motors.detector_polar)
+        return self.metadata.relative_polar + self.motors.detector_polar
+
+    @property
+    def pixel_azimuthal_angle_sample_frame(self):
+        """
+        Returns the azimuthal angle at each pixel, where the coordinate system
+        is the typical synchrotron coordinate system, but tied to the sample
+        (so with omega ≠ 0 or chi ≠ 0, the frame rotates with the sample).
+        """
+        return self.metadata.relative_azimuth + self.motors.detector_azimuth
 
     @property
     def q_out(self) -> np.ndarray:
@@ -104,18 +96,24 @@ class Image:
         """
         # We need num_x_pixels, num_y_pixels, 3 to be our shape.
         # Note that we need the extra "3" to store qx, qy, qz (3d vector).
-        desired_shape = tuple(list(self._raw_data.shape).append(3))
+        desired_shape = tuple(list(self._raw_data.shape) + [3])
         delta_q = np.zeros(desired_shape)
 
+        # Optimized trig calculations. One day, these should be done in C via
+        # lookup tables for maximum speed.
+        cos_azimuth = np.cos(self.pixel_azimuthal_angle_sample_frame)
+        sin_azimuth = np.sqrt(1 - cos_azimuth**2)
+        cos_polar = np.cos(self.pixel_polar_angle_sample_frame)
+        sin_polar = np.sqrt(1 - cos_polar**2)
         # Now set the elements of the delta q matrix element.
         # First set all the delta_q_x values, then delta_q_y, then delta_q_z.
-        cos_phi = np.cos(self.pixel_azimuth_lab_frame)
-        sin_phi = np.sqrt(1 - cos_phi**2)
-        cos_theta = np.cos(self.pixel_polar_lab_frame)
-        sin_theta = np.sqrt(1 - cos_theta**2)
-        delta_q[:, :, 0] = sin_phi
-        delta_q[:, :, 1] = cos_phi * sin_theta
-        delta_q[:, :, 2] = cos_phi * cos_theta-1
+        # Note that these are just q_out for now.
+        delta_q[:, :, 0] = sin_polar * sin_azimuth
+        delta_q[:, :, 1] = cos_polar
+        delta_q[:, :, 2] = sin_polar * cos_azimuth
+
+        # delta_q = q_out - q_in; finally, give it the correct length.
+        delta_q -= self.motors.incident_beam
         delta_q *= self.metadata.q_incident_lenth
 
         self._delta_q = delta_q

src/RSMapper/motors.py
@@ -108,7 +108,8 @@ class Motors:
     @property
     def detector_polar(self) -> float:
         """
-        Returns the detector's spherical polar theta value.
+        Returns the detector's spherical polar theta value in the sample's
+        frame.
         """
         # Call the appropriate function for the instrument in use.
         return getattr(self, f"_{self.metadata.instrument}_detector_polar")()
@@ -116,25 +117,9 @@ class Motors:
     @property
     def detector_azimuth(self) -> float:
         """
-        Returns the detector's spherical polar phi value.
+        Returns the detector's spherical polar phi value in the sample's frame.
         """
         return getattr(self, f"_{self.metadata.instrument}_detector_azimuth")()
-
-    @property
-    def sample_polar(self) -> float:
-        """
-        Returns the spherical polar polar angle in a coordinate system anchored
-        to the sample.
-        """
-        return getattr(self, f"_{self.metadata.instrument}_sample_polar")()
-
-    @property
-    def sample_azimuth(self) -> float:
-        """
-        Returns the spherical polar azimuthal angle in a coordinate system
-        anchored to the sample.
-        """
-        return getattr(self, f"_{self.metadata.instrument}_sample_azimuth")()
 
     @property
     def incident_beam(self) -> np.ndarray:

tests/test_image.py
@@ -8,18 +8,41 @@ clever fixture.
 
 # pylint: disable=protected-access
 
-from copy import deepcopy
 from typing import Tuple, List
+
+import numpy as np
+from numpy.testing import assert_allclose
 
 
 from RSMapper.image import Image
 from RSMapper.metadata import Metadata
-from RSMapper.scan import Scan
 
 
 def test_data(i10_parser_output_01: Tuple[List[Image], Metadata]):
     """
     Make sure that image_instance.data is properly normalized.
     """
+    images, metadata = i10_parser_output_01
+    assert (images[0].data == images[0]._raw_data/metadata.solid_angles).all()
+
+
+def test_delta_q_01(i10_parser_output_01: Tuple[List[Image], Metadata]):
+    """
+    Make sure that the scattering vectors are being correctly calculated. This
+    is tricky to do exactly, but we can get pretty close by grabbing the
+    brightest pixel and assuming that we scatter through the (100) to get there.
+    """
     images, _ = i10_parser_output_01
-    image = deepcopy(images[0])
+
+    # We should be on the Bragg peak in the centre image.
+    image = images[70]
+
+    # Brightest pixel should be on the Bragg peak.
+    max_intensity_pixel = np.where(image.data == np.max(image.data))
+    max_intensity_q = np.linalg.norm(image.delta_q[max_intensity_pixel])
+    wavelength = 1/max_intensity_q
+
+    # Published value of CSO unit cell length is 8.931 Å.
+    # Make sure that the brightest pixel corresponds to scattering from the
+    # (100) of a crystal whose lattice constant is within 1% of this value.
+    assert_allclose(wavelength, 8.931, rtol=1e-2)