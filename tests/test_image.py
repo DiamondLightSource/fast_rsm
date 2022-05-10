475cd965807b8150169e59620c26e4219a52d4dd
Brearton, Richard (DLSLtd,RAL,LSCI) <richard.brearton@diamond.ac.uk>
2022-05-10 15:46:15 +0100

    Tested binning in reciprocal space maps

--- tests/test_image.py
+++ tests/test_image.py
@@ -8,13 +8,14 @@ clever fixture.
 
 # pylint: disable=protected-access
 
-from typing import Tuple, List
+from time import time
 
 import numpy as np
 from numpy.testing import assert_allclose
 
 from diffraction_utils import Frame
 
+from RSMapper.binning import linear_bin
 from RSMapper.scan import Scan
 
 
@@ -47,3 +48,32 @@ def test_delta_q_01(i10_scan: Scan):
     # Make sure that the brightest pixel corresponds to scattering from the
     # (100) of a crystal whose lattice constant is within 1% of this value.
     assert_allclose(wavelength, 8.931, rtol=1e-2)
+
+
+def test_binning(i10_scan: Scan):
+    """
+    Make sure that no data is being lost when binning is carried out. This test
+    is implicitly testing _fix_intensity_geometry and _fix_delta_q_geometry.
+    """
+    image = i10_scan.images[67]
+    frame = Frame(Frame.hkl, i10_scan.metadata.diffractometer)
+    delta_q = image.delta_q(frame)
+
+    min0 = np.min(delta_q[:, :, 0])
+    min1 = np.min(delta_q[:, :, 1])
+    min2 = np.min(delta_q[:, :, 2])
+    max0 = np.max(delta_q[:, :, 0])
+    max1 = np.max(delta_q[:, :, 1])
+    max2 = np.max(delta_q[:, :, 2])
+
+    step = np.array([(max0 - min0)/20, (max1 - min1)/20, (max2 - min2)/100])
+    start = np.array([min0, min1, min2])
+    stop = np.array([max0, max1, max2])
+
+    # Try to bin. Add an assert 1 == 2 to see single threaded bin performance.
+    time1 = time()
+    binned = linear_bin(delta_q, image.data, start, stop, step)
+    time2 = time()
+    print(time2 - time1)
+
+    assert_allclose(np.sum(binned), np.sum(image.data))
