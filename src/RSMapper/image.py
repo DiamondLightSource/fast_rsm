aebeeb0a77b811da5ab6a1aea876937c11711022
Brearton, Richard (DLSLtd,RAL,LSCI) <richard.brearton@diamond.ac.uk>
2022-06-09 15:49:57 +0100

    Big change to the implementation of q_vector calculation

--- src/RSMapper/image.py
+++ src/RSMapper/image.py
@@ -113,14 +113,13 @@ class Image:
         # Now return the azimuthal angle at each pixel.
         return self.metadata.relative_azimuth + detector_vector.azimuthal_angle
 
-    def delta_q(self, frame: Frame) -> None:
+    def q_vectors(self, frame: Frame) -> None:
         """
         Calculates the wavevector through which light had to scatter to reach
         every pixel on the detector in a given frame of reference.
 
-        This is the most performance critical part of the code. As a result,
-        some of the maths is written out in weird ways. Trig identities are used
-        to save time wherever possible.
+        First, we calculate the wavevector of the outgoing light at each pixel.
+        This is done using basic vector algebra.
         """
         # Make sure that our frame of reference has the correct index.
         frame.scan_index = self.index
@@ -128,22 +127,56 @@ class Image:
         # We need num_x_pixels, num_y_pixels, 3 to be our shape.
         # Note that we need the extra "3" to store qx, qy, qz (3d vector).
         desired_shape = tuple(list(self._raw_data.shape) + [3])
-        delta_q = np.zeros(desired_shape)
+        k_out_array = np.zeros(desired_shape)
 
-        # Optimized trig calculations.
-        cos_azimuth = np.cos(self.pixel_azimuthal_angle(frame))
-        sin_azimuth = np.sqrt(1 - cos_azimuth**2)
-        cos_polar = np.cos(self.pixel_polar_angle(frame))
-        sin_polar = np.sqrt(1 - cos_polar**2)
-        # Now set the elements of the delta q matrix element.
-        # First set all the delta_q_x values, then delta_q_y, then delta_q_z.
-        # Note that these are just q_out for now.
-        delta_q[:, :, 0] = sin_polar * sin_azimuth
-        delta_q[:, :, 1] = cos_polar
-        delta_q[:, :, 2] = sin_polar * cos_azimuth
+        # Get a unit vector pointing towards the detector.
+        det_displacement = self.diffractometer.get_detector_vector(frame)
 
-        # delta_q = q_out - q_in; finally, give it the correct length.
-        delta_q -= self.diffractometer.get_incident_beam(frame).array
-        delta_q *= self.metadata.q_incident_lenth
+        # Make a unit vector that points upwards along the slow axis of the
+        # detector in this frame of reference.
+        det_vertical = self.diffractometer.get_detector_vertical(frame)
 
-        return delta_q
+        # Make a unit vector that points horizontally along the fast axis of the
+        # detector in this frame of reference.
+        det_horizontal = self.diffractometer.get_detector_horizontal(frame)
+
+        # Now we have an orthonormal basis: det_displacement points from the
+        # sample to the detector, det_vertical points vertically up the detector
+        # and det_horizontal points horizontally along the detector. To get a
+        # vector parallel to k_out, we can just work out the displacement from
+        # each pixel to the sample.
+        # This calculation is done component-by-component to match array shapes.
+        detector_distance = self.metadata.data_file.detector_distance
+        k_out_array[:, :, 0] = (
+            det_displacement.array[0]*detector_distance +
+            det_vertical.array[0]*self.metadata.vertical_pixel_distances +
+            det_horizontal.array[0]*self.metadata.horizontal_pixel_distances)
+        k_out_array[:, :, 1] = (
+            det_displacement.array[1]*detector_distance +
+            det_vertical.array[1]*self.metadata.vertical_pixel_distances +
+            det_horizontal.array[1]*self.metadata.horizontal_pixel_distances)
+        k_out_array[:, :, 2] = (
+            det_displacement.array[2]*detector_distance +
+            det_vertical.array[2]*self.metadata.vertical_pixel_distances +
+            det_horizontal.array[2]*self.metadata.horizontal_pixel_distances)
+
+        # We're going to need to normalize; this function bottlenecks.
+        norms_calculated = np.linalg.norm(k_out_array, axis=-1)
+
+        # Now we have to do some broadcasting magic. This is fastest when done
+        # explicitly. This part of the code is bottlenecking, so here goes.
+        norms = np.zeros_like(k_out_array)
+        # Scuffed, but the fastest way to get an array of the right shape.
+        norms[:, :, 0] = norms_calculated
+        norms[:, :, 1] = norms_calculated
+        norms[:, :, 2] = norms_calculated
+
+        # Right now, k_out_array[a, b] has units of meters for all a, b. We want
+        # k_out_array[a, b] to be normalized (elastic scattering). This can be
+        # done now that norms has been created because it has the right shape.
+        k_out_array /= norms
+
+        # Now simply subtract and rescale to get the q_vectors!
+        k_out_array -= self.diffractometer.get_incident_beam(frame).array
+        k_out_array *= self.metadata.q_incident_length
+        return k_out_array
