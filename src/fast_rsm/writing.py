7c55f0dc1cd1fdd8c1fa62f0b289896c2c3fe2b0
Brearton, Richard (DLSLtd,RAL,LSCI) <richard.brearton@diamond.ac.uk>
2022-06-24 15:59:00 +0100

    Changed edge calculation code

--- src/fast_rsm/writing.py
+++ src/fast_rsm/writing.py
@@ -16,9 +16,12 @@ def linear_bin_to_vtk(binned_data: np.ndarray,
     Takes binned data and saves it to a .vtk file.
     """
     # Coordinates
-    x_range = np.arange(start[0], stop[0] + step[0], step[0], dtype="float32")
-    y_range = np.arange(start[1], stop[1] + step[1], step[1], dtype="float32")
-    z_range = np.arange(start[2], stop[2] + step[2], step[2], dtype="float32")
+    x_range = np.arange(start[0], stop[0], step[0], dtype="float32")
+    x_range = np.array(list(x_range) + stop[0])
+    y_range = np.arange(start[1], stop[1], step[1], dtype="float32")
+    y_range = np.array(list(y_range) + stop[1])
+    z_range = np.arange(start[2], stop[2], step[2], dtype="float32")
+    z_range = np.array(list(z_range) + stop[2])
 
     gridToVTK(file_path, x_range, y_range, z_range,
               cellData={"Intensity": binned_data})
