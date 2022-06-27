47909d48b6a2ced258c6212cdd703cbabc02c196
Brearton, Richard (DLSLtd,RAL,LSCI) <richard.brearton@diamond.ac.uk>
2022-06-27 15:17:03 +0100

    Fixed previously buggy arange code

--- src/fast_rsm/writing.py
+++ src/fast_rsm/writing.py
@@ -17,11 +17,19 @@ def linear_bin_to_vtk(binned_data: np.ndarray,
     """
     # Coordinates
     x_range = np.arange(start[0], stop[0], step[0], dtype="float32")
-    x_range = np.array(list(x_range) + stop[0])
+    x_range = list(x_range)
+    x_range.append(stop[0])
+    x_range = np.array(x_range)
+
     y_range = np.arange(start[1], stop[1], step[1], dtype="float32")
-    y_range = np.array(list(y_range) + stop[1])
+    y_range = list(y_range)
+    y_range.append(stop[1])
+    y_range = np.array(y_range)
+
     z_range = np.arange(start[2], stop[2], step[2], dtype="float32")
-    z_range = np.array(list(z_range) + stop[2])
+    z_range = list(z_range)
+    z_range.append(stop[2])
+    z_range = np.array(z_range)
 
     gridToVTK(file_path, x_range, y_range, z_range,
               cellData={"Intensity": binned_data})
