3dab5dea8abe0fa21488925f2bf240e7ae5bd26b
Brearton, Richard (DLSLtd,RAL,LSCI) <richard.brearton@diamond.ac.uk>
2022-09-01 16:27:16 +0100

    Made an Experiment class to organise groups of scans

---
+++ src/fast_rsm/experiment.py
@@ -0,0 +1,181 @@
+"""
+This file contains the Experiment class, which contains all of the information
+relating to your experiment.
+"""
+
+import os
+from pathlib import Path
+from typing import List, Tuple, Union
+
+import numpy as np
+
+from diffraction_utils import Frame
+
+from . import io
+from .meta_analysis import get_step_from_filesize
+from .scan import Scan
+from .writing import linear_bin_to_vtk
+
+
+def _remove_file(path: Union[str, Path]):
+    """
+    Removes a file if it exists. Doesn't do anything if it doesn't exist.
+    """
+    try:
+        os.remove(path)
+    except OSError:
+        pass
+
+
+class Experiment:
+    """
+    The Experiment class specifies all experimental details necessary to process
+    experimental data. This can be a list of file paths, a central pixel
+    location, details of the experimental geometry, the desired output, etc.
+
+    Experiments contain lot of tools that can be used to work out what they are.
+    For example, experiments can work out whether or not binning is required to
+    reduce data volumes, and if so, generate finite difference volumes in which
+    the data can be optimally stored.
+
+    Attrs:
+        scans:
+            A list of all of the scans explored in this experiment.
+    """
+
+    def __init__(self, scans: List[Scan]) -> None:
+        self.scans = scans
+        self._data_file_names = []
+        self._normalisation_file_names = []
+
+    def _clean_temp_files(self):
+        """
+        Removes all temporary files.
+        """
+        for path in self._data_file_names:
+            _remove_file(path)
+        for path in self._normalisation_file_names:
+            _remove_file(path)
+
+    def binned_reciprocal_space_map(self,
+                                    num_threads: int,
+                                    map_frame: Frame,
+                                    output_file_name: str = "mapped_data",
+                                    min_intensity_mask: float = None,
+                                    output_file_size: float = 100):
+        """
+        Carries out a binned reciprocal space map for this experimental data.
+
+        Args:
+            num_threads:
+                How many threads should be used to carry out this map? You
+                should probably set this to however many threads are available
+                on your machine.
+            map_frame:
+                An instance of diffraction_utils.Frame that specifies what
+                frame of reference and coordinate system you'd like your map to
+                be carried out in.
+            output_file_name:
+                What should the final file be saved as?
+            output_file_size:
+                The desired output file size, in units of MB.
+                Defaults to 100 MB.
+        """
+        # Compute the optimal finite differences volume.
+        start, stop = self.q_bounds(map_frame)
+        step = get_step_from_filesize(start, stop, output_file_size)
+
+        # Carry out the maps.
+        for scan in self.scans:
+            rsmap, counts = scan.binned_reciprocal_space_map(
+                map_frame, start, stop, step, min_intensity_mask, num_threads)
+
+            scan_unique_path = scan.metadata.data_file.local_path
+            data_name = output_file_name + "_data_" + scan_unique_path
+            norm_name = output_file_name + "_norm_" + scan_unique_path
+
+            # Save the map and the normalisation array.
+            np.save(data_name, rsmap)
+            np.save(norm_name, counts)
+
+            # Store a record of where this has been saved.
+            self._data_file_names.append(data_name)
+            self._normalisation_file_names.append(norm_name)
+
+        # Combine the maps and normalise. TODO: parallelise this.
+        total_map = np.zeros_like(rsmap, dtype=np.float32)
+        total_counts = np.zeros_like(counts, dtype=np.float32)
+        for i, map_name in enumerate(self._data_file_names):
+            total_map += np.load(map_name + '.npy')
+            total_counts += np.load(self._normalisation_file_names[i] + '.npy')
+
+        normalised_map = total_map / total_counts
+        linear_bin_to_vtk(normalised_map, output_file_name, start, stop, step)
+
+        # Finally, remove all of the random .npy files we created along the way.
+        self._clean_temp_files()
+
+    def q_bounds(self, frame: Frame) -> Tuple[np.ndarray]:
+        """
+        Works out the region of reciprocal space sampled by every scan in this
+        experiment. This is reasonably performant, but should really be
+        parallelised at some point.
+
+        Returns:
+            (start, stop)
+        """
+        # Get a start and stop value for each scan.
+        starts, stops = [], []
+        for scan in self.scans:
+            start, stop = scan.q_bounds(frame)
+            starts.append(start)
+            stops.append(stop)
+
+        # Return the min of the starts and the max of the stops.
+        starts, stops = np.array(starts), np.array(stops)
+        return np.min(starts, axis=0), np.max(stops, axis=0)
+
+    @classmethod
+    def from_i07_nxs(cls,
+                     nexus_paths: List[Union[str, Path]],
+                     beam_centre: Tuple[int],
+                     detector_distance: float,
+                     setup: str,
+                     path_to_data: str = ''):
+        """
+        Generates an instance of Experiment from paths to nexus files obtained
+        from the i07 beamline. Also requires a few other essential pieces of
+        information to uniquely specify the experimental setup (because you
+        could be doing basically anything on i07...)
+
+        Args:
+            nexus_paths:
+                Path to the nexus files containing all of the scans' metadata.
+                If the experiment consists of only one scan, then you can pass
+                a single string/Path in place of a list of strings/paths.
+            beam_centre:
+                A (y, x) tuple of the beam centre, measured in the usual image
+                coordinate system, in units of pixels.
+            detector_distance:
+                The distance between the sample and the detector.
+            setup:
+                What was the experimental setup? Can be "vertical", "horizontal"
+                or "DCD".
+            path_to_data:
+                Path to the directory in which the images are stored. Defaults
+                to '', in which case a bunch of reasonable directories will be
+                searched for the images.
+
+        Returns:
+            Corresponding instance of Experiment.
+        """
+        # Make sure that we have a list, in case we just received a single path.
+        if isinstance(nexus_paths, (str, Path)):
+            nexus_paths = [nexus_paths]
+
+        # Instantiate all of the scans.
+        scans = [
+            io.from_i07(x, beam_centre, detector_distance, setup, path_to_data)
+            for x in nexus_paths]
+
+        return cls(scans)
