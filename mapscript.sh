ccde3acaef6a340e909f352a57eff53e59990313
Mousley <philip.mousley@diamond.ac.uk>
2023-06-08 12:01:03 +0100

    Create mapscript.sh
    added mapscript.sh with new SLURM job commands

---
+++ mapscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --partition cs05r 
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=40
+#SBATCH --mem-per-cpu=2500 
+#SBATCH --job-name=fast-rsm
+ 
+/dls_sw/apps/fast_rsm/current/conda_env/bin/python map.py
+
+
