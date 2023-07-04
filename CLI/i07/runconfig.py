f22ebd3c94e3aa2786dc0cf62e8c8302c81caaa7
Philip Mousley <philipmousley@diamond.ac.uk>
2023-07-04 16:50:04 +0100

    testing parsing config

--- CLI/i07/runconfig.py
+++ CLI/i07/runconfig.py
@@ -17,7 +17,7 @@ from fast_rsm.scan import Scan
 import argparse
 import os
 from yaml import load, dump, Loader
-
+from pathlib import Path
 
 if __name__ == "__main__":
 
@@ -31,12 +31,16 @@ if __name__ == "__main__":
         "Path to the YAML file with configuration settings. "
     )
     parser.add_argument("-y", "--yaml_path", help=HELP_STR)
+    
     HELP_STR = (
         "Scan numbers to be mapped into one reciprocal volume"
     )
-    parser.add_argument("-s", "--scan_nums", help=HELP_STR, type=int,
-                        default=os.environ.get("NUM_BINS"))
+    parser.add_argument("-s", "--scan_nums", help=HELP_STR, type=int)
 
+    HELP_STR = (
+        "Path to the directory for saving output files to. "
+    )
+    parser.add_argument("-o", "--out_path", help=HELP_STR)
 
     # Extract the arguments from the parser.
     args = parser.parse_args()
@@ -48,9 +52,36 @@ if __name__ == "__main__":
     EXP_N=recipe['visit']['experiment_number']
     YEAR=recipe['visit']['year']#
     SCANS=args.scan_nums
+    OUTDIR=args.out_path
 
-    f=open(r"C:\Users\rpy65944\Documents\testjobtemplate_nocom.py")
-    lines=f.readlines()
+    yaml_file=r"testconfig.yaml"
+    y_file = open(yaml_file, 'r', encoding='utf-8')
+    recipe = load(y_file, Loader=Loader)
+    y_file.close()
+
+    i=1
+    save_file_name = f"job_scan_{SCANS[0]}_{i}.py"
+    save_path = Path(OUTDIR)/Path(save_file_name)
+    # Make sure that this name hasn't been used in the past.
+    while (os.path.exists(str(save_path))):
+        i += 1
+        save_file_name = f"job_scan_{SCANS[0]}_{i}.py"
+        save_path = Path(OUTDIR)/Path(save_file_name)
+        if i > 1e7:
+            raise ValueError(
+                "naming counter hit limit therefore exiting ")
+
+    f=open(save_path,'w')
+    for line in lines:
+        if '$' in line:
+            phrase=line[line.find('$'):line.find('}')+1]
+            outphrase=phrase.strip('$').strip('{').strip('}')
+            outline=line.replace(phrase,str(locals()[f'{outphrase}']))
+            print(outline)
+            f.write(outline)
+        else:
+            f.write(line)
     f.close()
+
 
 
