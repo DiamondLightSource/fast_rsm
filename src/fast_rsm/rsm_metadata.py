235c1c66810a3823c8f6b7ff22a0db0188a9ed4e
Philip Mousley <philipmousley@diamond.ac.uk>
2023-08-23 15:36:44 +0100

    add debug line for horizontal offsets

--- src/fast_rsm/rsm_metadata.py
+++ src/fast_rsm/rsm_metadata.py
@@ -321,6 +321,7 @@ class RSMMetadata:
 
         # Save this value to an array with the same shape as the images.
         self._vertical_pixel_offsets = np.zeros(image_shape, np.float32)
+        
         for i, pixel_offset in enumerate(pixel_offsets):
             self._vertical_pixel_offsets[i, :] = pixel_offset
 
@@ -342,7 +343,7 @@ class RSMMetadata:
         pixel_offsets = np.arange(num_x_pixels-1, -1, -1)
         x_beam_centre = self.beam_centre[1]
         pixel_offsets -= ((num_x_pixels-1) - x_beam_centre)
-
+        print(f'length pixel horiz offsets: {len(pixel_offsets)}')
         # Save this value to an array with the same shape as the images.
         self._horizontal_pixel_offsets = np.zeros(image_shape, np.float32)
         for i, pixel_offset in enumerate(pixel_offsets):
