e6f464f46fa38fff315178906acd08161d2b5272
Philip Mousley <philipmousley@diamond.ac.uk>
2023-08-23 12:56:47 +0100

    edit debug lines

--- src/fast_rsm/image.py
+++ src/fast_rsm/image.py
@@ -237,9 +237,9 @@ class Image:
             vertical = np.flip(vertical, axis=0)
             horizontal = horizontal.transpose()
             horizontal = np.flip(horizontal, axis=0)
-        print(f'vertical shape= {vertical.shape()}')
-        print(f'horizontal shape= {horizontal.shape()}')
-        print(f'k_out shape ={k_out_array.shape()}')
+        print(f'vertical shape= {np.shape(vertical)}')
+        print(f'horizontal shape= {np.shape(horizontal)}')
+        print(f'k_out shape ={np.shape(k_out_array)}')
         k_out_array[i, j, 0] = (
             det_displacement.array[0]*detector_distance +
             det_vertical.array[0]*vertical[i, j] +
