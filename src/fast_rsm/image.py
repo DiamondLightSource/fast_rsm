7ea39e504a7eb21a42b0710af29c60f59c1a9cbd
Philip Mousley <philipmousley@hotmail.com>
2024-02-14 09:33:50 +0000

    add in normalisation to count time for image data

--- src/fast_rsm/image.py
+++ src/fast_rsm/image.py
@@ -140,6 +140,17 @@ class Image:
         except AttributeError:
             pass
 
+        #normalise image data to count time
+        detectorname=self.metadata.data_file._parse_detector_name()
+        scan_entry=self.metadata.diffractometer.data_file.nxfile
+        count_time=scan_entry.entry.instrument[f'{detectorname}'].count_time[self.index]
+        try:
+            arr /= count_time
+        except AttributeError:
+            print('could not normalise to count time')
+            pass
+        
+
         # If there are pixels to mask, mask them.
         if self.metadata.mask_pixels is not None:
             arr[self.metadata.mask_pixels] = np.nan
