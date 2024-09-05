f8b72471bcf348385ec456655d0bc94c05b4476b
Philip Mousley <philipmousley@hotmail.com>
2024-09-05 09:43:00 +0100

    re-organise the example_exp_setup file

--- CLI/i07/example_exp_setup.py
+++ CLI/i07/example_exp_setup.py
@@ -18,9 +18,9 @@ setup = 'horizontal'
 #which experimental hutch was used 0 = unsure, 1= experimental hutch 1, 2=experimental hutch 2
 experimental_hutch=1
 # Set this to the directory path where your files are saved, note you will need to include any subdirectories in this path
-local_data_path = 
+local_data_path =    # '/dls/i07/data/2024/##experiment-number##/##subfolder##
 # Set this to the path where you want the output from the data processing to be saved
-local_output_path =  '/dls/i07/data/2024/cm37245-1/PhilMousley_testing/output'
+local_output_path =  #'/dls/i07/data/2024/##experiment-number##/processing'
 
 
 # The beam centre, as can be read out from GDA, in pixel_x, pixel_y.
@@ -30,8 +30,18 @@ beam_centre = (119,1564)
 # distance between the receiving slit and the detector). Units of meters.
 detector_distance = 0.18
 
+# Are you using the DPS system?
+using_dps = False
+# The DPS central pixel locations are not typically recorded in the nexus file.
+# NOTE THAT THIS SHOULD BE THE CENTRAL PIXEL FOR THE UNDEFLECTED BEAM.
+# UNITS OF METERS, PLEASE (everything is S.I., except energy in eV).
+dpsx_central_pixel = 0
+dpsy_central_pixel = 0
+dpsz_central_pixel = 0
 
 #========CALCULATION INFORMATION==========
+
+#**************FULL RECIPROCAL VOLUME -  CRYSTAL TRUNCATION RODS, HKL MAPS etc
 # Set this to True if you would like each image to be mapped independently.
 # If this is False, all images in all scans will be combined into one large
 # reciprocal space map.
@@ -47,16 +57,6 @@ output_file_size = 50
 # This is for loading into binoculars. If set to false, .npy and .vtr files
 # will be saved, for manual analysis and paraview, respectively.
 save_binoculars_h5 = True
-
-# Are you using the DPS system?
-using_dps = False
-
-# The DPS central pixel locations are not typically recorded in the nexus file.
-# NOTE THAT THIS SHOULD BE THE CENTRAL PIXEL FOR THE UNDEFLECTED BEAM.
-# UNITS OF METERS, PLEASE (everything is S.I., except energy in eV).
-dpsx_central_pixel = 0
-dpsy_central_pixel = 0
-dpsz_central_pixel = 0
 
 # Note: THESE CAN BE HAPPILY AUTO CALCULATED.
 # These take the form:
@@ -74,10 +74,13 @@ volume_step = None
 load_from_dat = False
 
 
+
+#********GIWAXS WITH MOVING DETECTOR AND LARGE NUMBER OF IMAGES
 #if calculating pyfai integration on scan with moving detector and large number of images need to 
 # specify range of q or 2th so that number of bins can be calculated
 radialrange=(0,60)
 radialstepval=0.01
+qmapbins=(350,1050)
 
 #===========MASKING=============
 #add path to edfmaskfile created with pyFAI gui accessed via 'makemask' option in fast_rsm
@@ -113,6 +116,9 @@ mmask_regions = [mask_1, mask_2]
 # creating masks (which is really useful for generating masks from -ve numbers).
 min_intensity = 0.
 
+#=======OPTIONS FOR SKIPPING IMAGES IF ISSUES ARE PRESENT
+#CHOOSE SCANS WHICH HAVE IMAGES TO SKIP, AND THEN SPECIFY WHICH IMAGES WITHIN THOSE SCANS NEED TO BE SKIPPED 
+#I.E. A LIST OF IMAGES TO SKIP FOR EACH SCAN VALUE IN SKIPSCANS
 skipscans=[1235,\
            1237]
 
@@ -138,8 +144,8 @@ skipimages=[[4,12],\
 # 'large_moving_det' - utilise MultiGeometry option in pyFAI for scan with a moving detector and a 
 #                       large number of images (~1000s), outputs: I, Q, two theta, caked image 
 #
-process_outputs=['large_moving_det']
-qmapbins=(350,1050)
+process_outputs=[]#'large_moving_det' ,'qperp_qpara_map', 'pyfai_1D','curved_projection_2D','full_reciprocal_map'
+
 
 # The scan numbers of the scans that you want to use to produce this reciprocal
 # space map. 
