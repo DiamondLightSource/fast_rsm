40b2de688b9e401d0baeaeb0fe4541e3859b413e
P-Mousley <philipmousley@hotmail.com>
2024-09-27 09:40:52 +0100

    try to fix circular importing

--- src/fast_rsm/__init__.py
+++ src/fast_rsm/__init__.py
@@ -5,3 +5,9 @@ package much simpler and is standard practice in python packages.
 
 __version__ = '0.0.1'
 
+from .data_file import *
+from .diffractometer_base import *
+from .frame_of_reference import *
+from .io import *
+from .region import *
+from .vector import *
