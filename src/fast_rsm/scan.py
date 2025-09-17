45705552762ab861ed31f5381552db30617c4c79
p-mousley <philipmousley@hotmail.com>
2025-09-17 14:26:19 +0100

    edit name of logger

--- src/fast_rsm/scan.py
+++ src/fast_rsm/scan.py
@@ -32,7 +32,7 @@ from fast_rsm.rsm_metadata import RSMMetadata
 from fast_rsm.writing import linear_bin_to_vtk
 
 from fast_rsm.logging_config import get_frsm_logger
-logger=get_frsm_logger(__name__)
+logger=get_frsm_logger("fastrsm")
 
 
 lock=None
