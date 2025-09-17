46dabf32ac36249d2f9b0e63981284de9d9e5dae
p-mousley <philipmousley@hotmail.com>
2025-09-17 14:57:24 +0100

    prevent multiple logger handlers

--- src/fast_rsm/logging_config.py
+++ src/fast_rsm/logging_config.py
@@ -24,23 +24,26 @@ def get_frsm_logger(name: str):
     # Create fast_rsm logger
     logger = logging.getLogger(name)
     logging.basicConfig(level=logging.WARNING)
-    if _logging_enabled ==1:
-        log_path = os.path.join('/dls/science/groups/das/ExampleData/i07/fast_rsm_example_data', 'debug.log')
-        #Set root logger to WARNING to suppress third-party debug/info logs
-        logger.setLevel(logging.INFO)
-        # Add a rotating file handler to fast_rsm logger only
-        file_handler = logging.handlers.RotatingFileHandler(log_path, maxBytes=500000, backupCount=1 )
-        file_handler.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
 
-        print(f'logging at {log_path}')
-        logger.info("test info line")
-        logger.debug("test debug line")
-    else:
-        print("logging disabled")
-        logger.disabled=True
+     # Only configure once
+    if not logger.hasHandlers():
+        if _logging_enabled ==1:
+            log_path = os.path.join('/dls/science/groups/das/ExampleData/i07/fast_rsm_example_data', 'debug.log')
+            #Set root logger to WARNING to suppress third-party debug/info logs
+            logger.setLevel(logging.INFO)
+            # Add a rotating file handler to fast_rsm logger only
+            file_handler = logging.handlers.RotatingFileHandler(log_path, maxBytes=500000, backupCount=1 )
+            file_handler.setLevel(logging.DEBUG)
+            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+
+            print(f'logging at {log_path}')
+            logger.info("test info line")
+            logger.debug("test debug line")
+        else:
+            print("logging disabled")
+            logger.disabled=True
 
 
     return logger
