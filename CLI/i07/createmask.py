795dd112477c7fbcf3f0de9807b3f72360e6f08f
pmousley <philipmousley@hotmail.com>
2025-10-02 14:50:42 +0100

    add in option for specifying image number in makemask

--- CLI/i07/createmask.py
+++ CLI/i07/createmask.py
@@ -14,7 +14,7 @@ import h5py
 from PIL import Image
 
 
-def get_im_path(directorypath, scan_number):
+def get_im_path(directorypath, scan_number,image_number):
     files = [file for file in os.listdir(f'{directorypath}') if '.nxs' in file]
     found_file = [file for file in files if str(
         scan_number) + '.nxs' in file][0]
@@ -22,7 +22,7 @@ def get_im_path(directorypath, scan_number):
     found_nexus = I07Nexus(filepath, directorypath)
     if found_nexus.has_hdf5_data == True:
         hf = h5py.File(found_nexus.local_hdf5_path)
-        imdata = hf[found_nexus.hdf5_internal_path][0]
+        imdata = hf[found_nexus.hdf5_internal_path][image_number]
         imout = Image.fromarray(imdata, mode='I')
         fname = found_nexus.local_hdf5_path.split('/')[-1].strip('.h5')
         home_dir = os.path.expanduser('~')
@@ -48,12 +48,15 @@ if __name__ == "__main__":
     parser.add_argument("-dir", "--dir_path", help=HELP_STR)
 
     HELP_STR = (
-        "Path to experiment directory where the nexus file is located "
+        "Scan which you want to create a mask for "
     )
     parser.add_argument("-s", "--scan_number", help=HELP_STR)
-    args = parser.parse_args()
-    directorypath = args.dir_path
-    scan_number = args.scan_number
 
-    impath = get_im_path(directorypath, scan_number)
+    HELP_STR = (
+        "Image from chosen scan to create a mask for, defaults to 0 "
+    )
+    parser.add_argument("-s", "--image_number", help=HELP_STR,default=0)
+    args = parser.parse_args()
+
+    impath = get_im_path(args.dir_path, args.scan_number,args.image_number)
     subprocess.run(['pyFAI-drawmask', f'{impath}'])
