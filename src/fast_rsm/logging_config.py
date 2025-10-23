cd63c47e2952437316f6d9daa98bc2a77fb59c1b
pmousley <philipmousley@hotmail.com>
2025-10-23 15:57:58 +0100

    edit logging handler

--- src/fast_rsm/logging_config.py
+++ src/fast_rsm/logging_config.py
@@ -1,11 +1,13 @@
 """
 custom module to contain methods for creating custom a custom logger
 """
+import shutil
+import os
 import logging
 import logging.config
 import getpass
 import concurrent_log_handler
-import shutil
+
 
 
 ERROR_LOG_DIR='/dls/science/groups/das/ExampleData/i07/fast_rsm_error_logs'
@@ -29,10 +31,32 @@ def get_debug_logger():
     """
     return logging.getLogger('fastrsm_debug')
 
+def get_error_logger():
+    """
+    return already initialised debug logger
+    """
+    return logging.getLogger('fastrsm_error')
+
 def log_error_info(jobfile,slurmfile,error_logger):
     """
     pass error info to logger
     """
-    shutil.copy2(slurmfile,ERROR_LOG_DIR)
-
+    copy_path=shutil.copy2(slurmfile,ERROR_LOG_DIR)
+    os.chmod(copy_path, 0o777)
     error_logger.error(f"{getpass.getuser()}\t{jobfile}\t{slurmfile}")
+
+def close_logging():
+    '''
+    closes all logging handlers and then shutsdown logging
+    '''
+    e_logger=get_error_logger()
+    # Close all handlers
+    for handler in e_logger.handlers[:]:
+        handler.close()
+
+    # Shutdown logging
+    logging.shutdown()
+
+    lock_file=f'{ERROR_LOG_DIR}/.__errors.lock'
+    if os.path.exists(lock_file):
+        os.remove(lock_file)
