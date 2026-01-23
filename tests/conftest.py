f6c77a49f1842d50a2847f934407bc0e7771ff07
pmousley <philipmousley@hotmail.com>
2026-01-23 17:03:29 +0000

    add in fixture for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,6 +15,15 @@ from fast_rsm.config_loader import experiment_config
 
 
 @fixture
+def test_default_config():
+    """
+    returns a default_config dict with scan_numbers=[1234]    
+    """
+    defaultconfig=experiment_config([1234])
+    defaultconfig['full_path']=__file__
+    return defaultconfig
+
+@fixture
 def path_to_resources():
     """
     Returns the path to the test resources folder.
