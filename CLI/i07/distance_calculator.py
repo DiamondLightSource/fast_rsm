cc9afa14ddfc13cdfc80297fd81a000986b20549
pmousley <philipmousley@hotmail.com>
2026-04-15 16:23:17 +0100

    update the distance calculator

--- CLI/i07/distance_calculator.py
+++ CLI/i07/distance_calculator.py
@@ -5,6 +5,7 @@ from operator import itemgetter
 from pathlib import Path
 
 import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 from diffraction_utils.diffractometers.diamond_i07 import I07Diffractometer
 from diffraction_utils.io import I07Nexus
@@ -45,7 +46,7 @@ class profpeak:
         self.imlist = imlist
 
     def update_lists(self, pos, im):
-        self.pos_list.append(pos[0])
+        self.pos_list.append(pos)
         self.imlist.append(im)
 
     def calc_shifts(self):
@@ -74,22 +75,33 @@ def findpeaks(data: np.ndarray):
         group = list(map(itemgetter(1), g))
         if len(group) <= 2:
             continue
-        np.append(ranges, (group[0] + group[-1]) / 2)
-    return ranges
+        ranges = np.append(ranges, (group[0] + group[-1]) / 2)
+
+    return ranges[::-1]
 
 
 def get_line_profiles(
-    loader: easyloader, selection: np.ndarray, central_pixel: int
+    loader: easyloader, selection: np.ndarray, central_pixel: int, axis: str
 ) -> np.ndarray:
 
+    start = central_pixel - 15
+    stop = central_pixel + 15
+    if axis == "x":
+        selectionslice = (slice(start, stop), slice(None))
+        axis_ind = 0
+        shape_ind = 1
+    else:
+        selectionslice = (slice(None), slice(start, stop))
+        axis_ind = 1
+        shape_ind = 0
+
     lineprofiles = np.zeros(
-        (len(selection), loader.diff.data_file.image_shape[1])
-    )  # loader.diff.data_file.image_shape[1]
+        (len(selection), loader.diff.data_file.image_shape[shape_ind])
+    )
+
     for line_ind, ind in enumerate(selection):
-        start = central_pixel - 10
-        stop = central_pixel + 10
-        section = loader.get_image(ind)[start:stop, :]
-        lineprofiles[line_ind] = np.sum(section, axis=0)
+        section = loader.get_image(ind)[selectionslice]
+        lineprofiles[line_ind] = np.sum(section, axis=axis_ind)
     return lineprofiles
 
 
@@ -97,27 +109,57 @@ def calc_dist(theta, Opp):
     return Opp / np.tan(np.radians(theta))
 
 
+class PeakSet:
+    def __init__(self, tracked_peaks: list[profpeak], finished_peaks: list[profpeak]):
+        self.track_peaks = tracked_peaks
+        self.finish_peaks = finished_peaks
+
+    def add_new_peak(self, peak, index):
+        self.track_peaks.append(
+            profpeak(startpos=peak, pos_list=[peak], imlist=[index])
+        )
+
+    def end_peak(
+        self,
+    ):
+        self.finish_peaks.append(self.track_peaks[0])
+        self.track_peaks = self.track_peaks[1:]
+
+    def plot_peaks(self):
+        fig, axs = plt.subplots(1, 2)
+        axlist = axs.ravel()
+        for peak in self.track_peaks:
+            axs[0].plot(peak.pos_list)
+        for peak in self.finish_peaks:
+            axs[1].plot(peak.pos_list)
+        plt.show()
+
+
 def identify_peaks(lineprofiles: np.ndarray) -> list:
-    track_peaks: list[profpeak] = []
-    finished_peaks: list[profpeak] = []
+    peak_set = PeakSet(tracked_peaks=[], finished_peaks=[])
     for line_ind, lineprof in enumerate(lineprofiles):
         peaks = np.argwhere(lineprof > (lineprof.mean() + 2.5 * lineprof.std()))
-        outpeaks = findpeaks(peaks)
+        outpeaks = findpeaks(peaks.ravel())
         for i, peak in enumerate(outpeaks):
-            if len(outpeaks) < len(track_peaks):
-                finished_peaks.append(track_peaks[0])
-                track_peaks = track_peaks[1:]
-            if (len(track_peaks) >= i + 1) and (
-                (line_ind - 1) in track_peaks[i].imlist
-            ):
-                track_peaks[i].update_lists(peak, line_ind)
+            if len(outpeaks) < len(peak_set.track_peaks):
+                peak_set.end_peak()
 
-            else:
-                track_peaks.append(
-                    profpeak(startpos=peak, pos_list=[peak], imlist=[line_ind])
-                )
+            if len(peak_set.track_peaks) < i + 1:
+                peak_set.add_new_peak(peak, line_ind)
+                continue
 
-    return [track_peaks, finished_peaks]
+            poslist_i = np.array(peak_set.track_peaks[i].pos_list)
+            if (peak > poslist_i[-1]) and (len(poslist_i) == 1):
+                peak_set.track_peaks[i].update_lists(peak, line_ind)
+                continue
+
+            if (peak < poslist_i[-1]) or ((peak - poslist_i[-1]) > 2 * poslist_i.std()):
+                peak_set.end_peak()
+                peak_set.add_new_peak(peak, line_ind)
+                continue
+            peak_set.track_peaks[i].update_lists(peak, line_ind)
+
+    return [peak_set.track_peaks, peak_set.finish_peaks]
 
 
 def fit_linear_models(peaks: list[profpeak]):
@@ -125,7 +167,7 @@ def fit_linear_models(peaks: list[profpeak]):
     slopes_err = np.array([])
     print("fitting linear models")
     for i, peak in enumerate(peaks):
-        yvals = np.array([pos[0] for pos in peak.pos_list])
+        yvals = np.array(peak.pos_list)
         xvals = np.arange(len(yvals))
         result = do_linear_fit(xvals, yvals)
         # axs.bar(i,result.params['line_slope'].value)
@@ -139,13 +181,17 @@ def fit_linear_models(peaks: list[profpeak]):
     return slopes, slope_meanerr
 
 
-def calc_distance(nexusfile: str, datapath: str, central_pixel: int, image_inds: tuple):
+def calc_distance(
+    nexusfile: str, datapath: str, central_pixel: int, image_inds: tuple, move_axis: str
+):
 
     loader = easyloader(nexusfile=nexusfile, datapath=datapath)
     selection = np.arange(image_inds[0], image_inds[1], 1)
-    line_profiles = get_line_profiles(loader, selection, central_pixel)
+    line_profiles = get_line_profiles(loader, selection, central_pixel, move_axis)
     tracked_peaks, finished_peaks = identify_peaks(line_profiles)
-    fullpeaks = [peak for peak in finished_peaks if len(peak.pos_list) > 10]
+    fullpeaks = [
+        peak for peak in finished_peaks + tracked_peaks if len(peak.pos_list) > 1
+    ]
     print(f"found {len(fullpeaks)} good tracked signals")
     slopes, slopes_meanerr = fit_linear_models(fullpeaks)
 
@@ -153,7 +199,6 @@ def calc_distance(nexusfile: str, datapath: str, central_pixel: int, image_inds:
     print(f"average_slope = {avslope} +/- {slopes_meanerr}")
 
     pixsize = loader.diff.data_file.pixel_size
-
     selected_angles = loader.diff.data_file.default_axis[selection]
     selected_angles_end = selected_angles[1:]
     ang_shift = np.array(
@@ -166,7 +211,7 @@ def calc_distance(nexusfile: str, datapath: str, central_pixel: int, image_inds:
     d1 = calc_dist(ang_shift.mean(), avslope * pixsize)
     d2 = calc_dist(ang_shift.mean(), (avslope + (slopes_meanerr)) * pixsize)
 
-    print(f"distance = {d1} +/- {d2 - d1}")
+    print(f"distance (m) = {d1} +/- {d2 - d1}")
 
 
 if __name__ == "__main__":
@@ -191,6 +236,11 @@ if __name__ == "__main__":
     HELP_STR = "index of image to end tracking peaks"
     parser.add_argument("-imend", "--end_image", type=int, help=HELP_STR)
 
+    HELP_STR = (
+        "axis of images that is the direction of detector movement, either x or y"
+    )
+    parser.add_argument("-axis", "--move_axis", type=str, help=HELP_STR)
+
     args = parser.parse_args()
     datapath = Path(args.dir_path)
     infile = datapath / f"i07-{args.scan_num}.nxs"
@@ -200,4 +250,5 @@ if __name__ == "__main__":
         datapath=str(datapath),
         central_pixel=int(args.cen_pix),
         image_inds=im_indices,
+        move_axis=args.move_axis,
     )
