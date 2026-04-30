bd559cb2d20f8f3c175f2f5105bbced8197116e3
pmousley <philipmousley@hotmail.com>
2026-04-30 08:11:13 +0100

    chore: record new branch in setup.py (mask_issue)

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-latest version created: add_ivschi in fast_rsm/
+latest version created: mask_issue in fast_rsm/
 The setup file. To install as a developer (only do this in a venv):
 
 python setup.py develop
