426b41e8f5a3c6b0e7e57bc81e980b36a6241a62
P-Mousley <philipmousley@hotmail.com>
2026-05-14 10:42:02 +0100

    chore: record new branch in setup.py (writing_module)

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-latest version created: add_ivschi in fast_rsm/
+latest version created: writing_module in fast_rsm/
 The setup file. To install as a developer (only do this in a venv):
 
 python setup.py develop
