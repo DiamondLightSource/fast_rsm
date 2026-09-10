6daed83cc5e7276e5997065e1922a7f7f829f3a5
P-Mousley <philipmousley@hotmail.com>
2026-09-10 14:06:26 +0100

    chore: record new branch in setup.py (tiff_dat_extract)

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-latest version created: parsing_detector in fast_rsm/
+latest version created: tiff_dat_extract in fast_rsm/
 The setup file. To install as a developer (only do this in a venv):
 
 python setup.py develop
